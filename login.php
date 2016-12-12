d77ac3b0a3b5619c1f09b0d307ebd114a05d407d
tpl <trphloc@gmail.com>
2016-12-12 16:20:42 +0000

    login function

--- login.php
+++ login.php
@@ -1,7 +1,25 @@
 <?php
-$out = '{';
-$out = $out . '"user":"' . $_POST["name"] . '",';
-$out = $out . '"token":"' . 'token123' . '"';
-$out = $out . '}';
-echo $out;
+$json = file_get_contents('php://input');
+$obj = json_decode($json, true);
+$user = $obj["username"];
+
+if(strcmp($user, "tpl") == 0){
+	$unixtime = time();
+	$out = '{';	
+	$out = $out . '"status":true,';
+	$out = $out . '"data":{"id":1,"username":"' . $user . '",';
+	$out = $out . '"token":"' . 'tokentpl' . '",';
+	$out = $out . '"loginDate":' . $unixtime . ',';
+	$out = $out . '"expiredDate":' . ($unixtime + (7*24*60*60)) . ',';        
+	$out = $out . '"isExpired":true},';
+        $out = $out . '"message":"login success"';        
+	$out = $out . '}';
+	echo $out;
+}else{
+	$out = '{';
+	$out = $out . '"status":false,';
+	$out = $out . '"data":"",';
+	$out = $out . '"message":"login fail"';        
+	echo $out;
+}
 ?>
